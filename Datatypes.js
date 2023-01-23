ce95f696967aa70b75eb6da16896e7e704314a52
DeverineMJames <122414664+DeverineMJames@users.noreply.github.com>
2023-01-23 07:20:40 -0500

    Add files via upload

---
+++ Datatypes.js
@@ -0,0 +1,55 @@
+// Strings
+let firstName= "Deverine";
+let LastName= "James";
+let Middle_Initial= "M";
+let last_Initial= "J";
+let bDay_Month= "January";  
+let bDay_Day= "forth";
+var favAnimals1= "none";
+const favInsect2= "Butterflies";
+const  favNumber= "7";
+var numofChildren= "one";
+
+//firstName = "dj";
+//console.log(firstName);
+//intergers
+let x = 123;    
+let y = 1234;   
+let d= x+y;
+let b= 3+6;
+var num=d+b;
+const num2= 71;
+const num3= 14;
+const numbers= num2+num3;
+var numbers2= numbers +numbers;
+var number3= numbers2+numbers2+ numbers;
+//console.log(number3);
+
+//array
+
+let DevCousins=["Zedan","Iverson","Seline","Raemona"];
+let DevFriends=["Stephanie","Precious","Mimi","Afiah","Terri"];
+const danaSisters=["Timara"];
+const danaBrothers=["0"];
+var familybDay=["June", "September","January", "April"];
+var parents=["mom","dad"];
+const favcars = ["Jeep", "BMW", "Honda"];
+const favFood = ["curry chicken", "shrimp alfredo", "Salad"];
+let favFashionBrand = ["Banana republic", "Fashionova", "Zara"];
+let favfruits = ["Watermelon", "Banana", "Pineapple"];
+DevNephews[0];
+DevNiece[0];
+// console.log();
+
+//objects
+let cellphone={name:"apple", type:"smart phone", color: "rosegold"};
+let myself={name:"Deverine", bodyType:"Atlethic", hairColor: "brown"};
+let car={name:"honda", type:"accord", color: "Gunmetal", year: 2019};
+const cousin = {firstName:"Zedan", age:18, eyeColor:"brown"};
+const daughter={firstName:"Trinity", age:6, skinColor:"brown", hairColor:"brown"};
+const sister={firstName:"timara", age:24, skinColor:"brown", hairColor:"brown"};
+const bestfriend={firstName:"Terri", age:35, skinColor:"brown", hairColor:"black"};
+const  = {name: "kelly",color: "brown",age: 3,goodGirl: true,};
+const {color: userColor ,make:userMake,model: userModel,year: userYear } = carDetails;
+let {title, make, model} = obj;
+console.log(car.name);
